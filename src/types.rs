4d110a9f70b1fc25a81f5ea65ca6e9132646ff22
Lorenzo Della Giustina <dellagiustina.lorenzo@gmail.com>
2024-09-09 16:27:24 +0200

    New types added

--- src/types.rs
+++ src/types.rs
@@ -6,14 +6,17 @@
 
 use fsds_rs_derive::IntoValue;
 use msgpack_rpc::Value;
-use std::{any::Any, ops::{Add, Div, DivAssign, Mul, MulAssign, Sub}};
+use std::{
+    any::Any,
+    ops::{Add, Div, DivAssign, Mul, MulAssign, Sub},
+};
 use struct_iterable::Iterable;
 
 /// Utility to convert any type to a `msgpack_rpc::Value`.
 fn any_to_value(value: &dyn Any) -> Value {
-    if let Some(value) = value.downcast_ref::<u32>() {
+    if let Some(value) = value.downcast_ref::<u64>() {
         Value::from(*value)
-    } else if let Some(value) = value.downcast_ref::<f32>() {
+    } else if let Some(value) = value.downcast_ref::<f64>() {
         Value::from(*value)
     } else if let Some(value) = value.downcast_ref::<bool>() {
         Value::from(*value)
@@ -39,7 +42,7 @@ pub enum ImageType {
 
 impl From<ImageType> for Value {
     fn from(value: ImageType) -> Self {
-        Value::from(value as u32)
+        Value::from(value as u64)
     }
 }
 
@@ -47,22 +50,22 @@ impl From<ImageType> for Value {
 /// VECTOR 3R ///
 /// --------- ///
 #[derive(Copy, Clone, Default, Iterable, IntoValue)]
-struct Vector3r {
-    x_val: f32,
-    y_val: f32,
-    z_val: f32,
+pub struct Vector3r {
+    x_val: f64,
+    y_val: f64,
+    z_val: f64,
 }
 
 impl Vector3r {
     pub fn nan_vector3r() -> Self {
         Self {
-            x_val: f32::NAN,
-            y_val: f32::NAN,
-            z_val: f32::NAN,
+            x_val: f64::NAN,
+            y_val: f64::NAN,
+            z_val: f64::NAN,
         }
     }
 
-    pub fn dot(&self, other: &Self) -> f32 {
+    pub fn dot(&self, other: &Self) -> f64 {
         self.x_val * other.x_val + self.y_val * other.y_val + self.z_val * other.z_val
     }
 
@@ -74,11 +77,11 @@ impl Vector3r {
         }
     }
 
-    pub fn get_length(&self) -> f32 {
+    pub fn get_length(&self) -> f64 {
         (self.x_val.powi(2) + self.y_val.powi(2) + self.z_val.powi(2)).sqrt()
     }
 
-    pub fn distance_to(&self, other: &Self) -> f32 {
+    pub fn distance_to(&self, other: &Self) -> f64 {
         (*self - *other).get_length()
     }
 }
@@ -107,16 +110,16 @@ impl Sub for Vector3r {
     }
 }
 
-impl DivAssign<f32> for Vector3r {
-    fn div_assign(&mut self, other: f32) {
+impl DivAssign<f64> for Vector3r {
+    fn div_assign(&mut self, other: f64) {
         self.x_val /= other;
         self.y_val /= other;
         self.z_val /= other;
     }
 }
 
-impl MulAssign<f32> for Vector3r {
-    fn mul_assign(&mut self, other: f32) {
+impl MulAssign<f64> for Vector3r {
+    fn mul_assign(&mut self, other: f64) {
         self.x_val *= other;
         self.y_val *= other;
         self.z_val *= other;
@@ -128,15 +131,15 @@ impl MulAssign<f32> for Vector3r {
 /// ----------- ///
 
 #[derive(Copy, Clone, Default, Iterable, IntoValue)]
-struct Quaternionr {
-    w_val: f32,
-    x_val: f32,
-    y_val: f32,
-    z_val: f32,
+pub struct Quaternionr {
+    w_val: f64,
+    x_val: f64,
+    y_val: f64,
+    z_val: f64,
 }
 
 impl Quaternionr {
-    pub fn new(w_val: f32, x_val: f32, y_val: f32, z_val: f32) -> Self {
+    pub fn new(w_val: f64, x_val: f64, y_val: f64, z_val: f64) -> Self {
         Self {
             w_val,
             x_val,
@@ -147,11 +150,69 @@ impl Quaternionr {
 
     pub fn nan_quaternionr() -> Self {
         Self {
-            w_val: f32::NAN,
-            x_val: f32::NAN,
-            y_val: f32::NAN,
-            z_val: f32::NAN,
+            w_val: f64::NAN,
+            x_val: f64::NAN,
+            y_val: f64::NAN,
+            z_val: f64::NAN,
         }
+    }
+
+    pub fn dot(&self, other: &Self) -> f64 {
+        self.w_val * other.w_val
+            + self.x_val * other.x_val
+            + self.y_val * other.y_val
+            + self.z_val * other.z_val
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        let mut diff = *self * *other - *other * *self;
+        diff /= 2.0;
+        diff
+    }
+
+    pub fn outer_product(&self, other: &Self) -> Self {
+        let mut double = self.inverse() * *other - other.inverse() * *self;
+        double /= 2.0;
+        double
+    }
+
+    pub fn rotate(&self, other: &Quaternionr) -> Result<Self, anyhow::Error> {
+        if other.get_length() == 1.0 {
+            return Ok(*other * *self * other.inverse());
+        }
+
+        Err(anyhow::anyhow!("Quaternion is not normalized"))
+    }
+
+    pub fn conjugate(&self) -> Self {
+        Self {
+            w_val: self.w_val,
+            x_val: -self.x_val,
+            y_val: -self.y_val,
+            z_val: -self.z_val,
+        }
+    }
+
+    pub fn star(&self) -> Self {
+        self.conjugate()
+    }
+
+    pub fn inverse(&self) -> Self {
+        let mut star = self.star();
+        star /= self.dot(self);
+
+        star
+    }
+
+    pub fn sgn(&self) -> Quaternionr {
+        let mut self_deref = *self;
+        self_deref /= self.get_length();
+
+        self_deref
+    }
+
+    pub fn get_length(&self) -> f64 {
+        (self.w_val.powi(2) + self.x_val.powi(2) + self.y_val.powi(2) + self.z_val.powi(2)).sqrt()
     }
 }
 
@@ -168,25 +229,96 @@ impl Add for Quaternionr {
     }
 }
 
+impl Sub for Quaternionr {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            w_val: self.w_val - other.w_val,
+            x_val: self.x_val - other.x_val,
+            y_val: self.y_val - other.y_val,
+            z_val: self.z_val - other.z_val,
+        }
+    }
+}
+
 impl Mul for Quaternionr {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        todo!()
+        let (t, x, y, z) = (self.w_val, self.x_val, self.y_val, self.z_val);
+        let (a, b, c, d) = (other.w_val, other.x_val, other.y_val, other.z_val);
+
+        Self {
+            w_val: a * t - b * x - c * y - d * z,
+            x_val: b * t + a * x + d * y - c * z,
+            y_val: c * t + a * y + b * z - d * x,
+            z_val: d * t + z * a + c * x - b * y,
+        }
     }
 }
 
+impl Div for Quaternionr {
+    type Output = Self;
 
+    fn div(self, other: Self) -> Self {
+        self * other.inverse()
+    }
+}
+
+impl DivAssign<f64> for Quaternionr {
+    fn div_assign(&mut self, other: f64) {
+        self.w_val /= other;
+        self.x_val /= other;
+        self.y_val /= other;
+        self.z_val /= other;
+    }
+}
 
 impl From<Vector3r> for Quaternionr {
     fn from(value: Vector3r) -> Self {
         Self {
-            w_val: 0.0,
-            x_val: value.x_val,
-            y_val: value.y_val,
-            z_val: value.z_val,
+            w_val: value.x_val,
+            x_val: value.y_val,
+            y_val: value.z_val,
+            z_val: 0.0,
         }
     }
+}
+
+/// ---- ///
+/// POSE ///
+/// ---- ///
+#[derive(Copy, Clone, Default, Iterable, IntoValue)]
+pub struct Pose {
+    position: Vector3r,
+    orientation: Quaternionr,
+}
+
+impl Pose {
+    pub fn new(posizion_val: Option<Vector3r>, orientation_val: Option<Quaternionr>) -> Self {
+        Self {
+            position: posizion_val.unwrap_or(Vector3r::nan_vector3r()),
+            orientation: orientation_val.unwrap_or(Quaternionr::nan_quaternionr()),
+        }
+    }
+
+    pub fn nan_pose() -> Self {
+        Self {
+            position: Vector3r::nan_vector3r(),
+            orientation: Quaternionr::nan_quaternionr(),
+        }
+    }
+}
+
+/// --------- ///
+/// GEO POINT ///
+/// --------- ///
+#[derive(Copy, Clone, Default, Iterable, IntoValue)]
+pub struct GeoPoint {
+    latitude: f64,
+    longitude: f64,
+    altitude: f64,
 }
 
 /// ------------- ///
@@ -211,17 +343,51 @@ impl<'a> Default for ImageRequest {
     }
 }
 
+/// -------------- ///
+/// IMAGE RESPONSE ///
+/// -------------- ///
+#[derive(Iterable, IntoValue)]
+pub struct ImageResponse {
+    image_data: (u8, f64),
+    camera_position: Vector3r,
+    camera_orientation: Quaternionr,
+    timestamp: u64, // TODO: SystemTime?
+    message: String,
+    pixels_as_float: f64,
+    compress: bool,
+    width: u64,
+    height: u64,
+    image_type: ImageType,
+}
+
+impl Default for ImageResponse {
+    fn default() -> Self {
+        Self {
+            image_data: (0, 0.0),
+            camera_position: Default::default(),
+            camera_orientation: Default::default(),
+            timestamp: 0,
+            message: "".to_string(),
+            pixels_as_float: 0.0,
+            compress: true,
+            width: 0,
+            height: 0,
+            image_type: ImageType::Scene,
+        }
+    }
+}
+
 /// ------------ ///
 /// CAR CONTROLS ///
 /// ------------ ///
 #[derive(Iterable, IntoValue)]
 pub struct CarControls {
-    pub throttle: f32,
-    pub steering: f32,
-    pub brake: f32,
+    pub throttle: f64,
+    pub steering: f64,
+    pub brake: f64,
     pub handbrake: bool,
     pub is_manual_gear: bool,
-    pub manual_gear: u32,
+    pub manual_gear: u64,
     pub gear_immediate: bool,
 }
 
@@ -231,8 +397,8 @@ impl Default for CarControls {
             throttle: Default::default(),
             steering: Default::default(),
             brake: Default::default(),
-            handbrake: Default::default(),
-            is_manual_gear: Default::default(),
+            handbrake: false,
+            is_manual_gear: false,
             manual_gear: Default::default(),
             gear_immediate: true,
         }
@@ -242,8 +408,8 @@ impl Default for CarControls {
 /// ---------------- ///
 /// KINEMATICS STATE ///
 /// ---------------- ///
-
-struct KinematicsState {
+#[derive(Iterable, IntoValue, Default)]
+pub struct KinematicsState {
     position: Vector3r,
     orientation: Quaternionr,
     linear_velocity: Vector3r,
@@ -251,3 +417,70 @@ struct KinematicsState {
     linear_acceleration: Vector3r,
     angular_acceleration: Vector3r,
 }
+
+/// ----------------- ///
+/// ENVIRONMENT STATE ///
+/// ----------------- ///
+#[derive(Iterable, IntoValue, Default)]
+pub struct EnvironmentState {
+    pub position: Vector3r,
+    pub geo_point: GeoPoint,
+    pub gravity: Vector3r,
+    pub air_pressure: f64,
+    pub temperature: f64,
+    pub air_density: f64,
+}
+
+/// -------------- ///
+/// COLLISION INFO ///
+/// -------------- ///
+#[derive(Iterable, IntoValue)]
+pub struct CollisionInfo {
+    pub has_collided: bool,
+    pub normal: Vector3r,
+    pub impact_point: Vector3r,
+    pub position: Vector3r,
+    pub penetration_depth: f64,
+    pub timestamp: f64, // TODO: SystemTime?
+    pub object_name: String,
+    pub object_id: u64,
+}
+
+/// --------- ///
+/// CAR STATE ///
+/// --------- ///
+#[derive(Iterable, IntoValue)]
+pub struct CarState {
+    pub speed: f64,
+    pub kinematics_estimated: KinematicsState,
+    pub timestamp: u64, // TODO: SystemTime?
+}
+
+/// ----------- ///
+/// POSITION 2D ///
+/// ----------- ///
+#[derive(Iterable, IntoValue, Default)]
+pub struct Position2D {
+    pub x_val: f64,
+    pub y_val: f64,
+}
+
+/// ------------- ///
+/// REFEREE STATE ///
+/// ------------- ///
+#[derive(Iterable, IntoValue, Default)]
+pub struct RefereeState {
+    pub doo_counter: u64,
+    pub laps: f64,
+    pub initial_position: Position2D,
+    pub cones: Vec<Position2D>,
+}
+
+// TODO:
+// ----------------- ///
+// PROJECTION MATRIX ///
+// ----------------- ///
+// #[derive(Iterable, IntoValue, Default)]
+// pub struct ProjectionMatrix {
+//     pub matrix: Vec<_>,
+// }
